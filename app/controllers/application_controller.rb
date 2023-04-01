3602550e0016e4a1bfc66904cbf3386397a6a411
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-31 20:10:44 -0500

    add authorization and validations to action controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :not_valid
+  rescue_from ActiveRecord::RecordNotFound, with: :no_route
+
+  before_action :authorize
+
+  private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  def not_valid(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def no_route
+    render json: {error: "Title not found"}, status: :not_found
+  end
+
+
 end
