e4e8013fbd08424dd3750f301364ad0de0eb860b
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-31 20:23:13 -0500

    add bottles controller actions

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -1,2 +1,31 @@
 class BottlesController < ApplicationController
+
+    def show
+        bottle = @current_user.bottles.find_by(id: params[:id])
+        render json: bottle, status: :found
+    end
+
+    def create
+        bottle = @current_user.bottles.create!(bottle_params)
+        render json: bottle, status: :created
+    end
+
+    def update
+        book = @current_user.books.find_by(id: params[:id])
+        book.update!(book_params)
+        render json: book
+    end
+    
+    def destroy
+        bottle = @current_user.bottles.find_by(id: params[:id])
+        bottle.destroy
+        head :no_content
+    end
+    
+    private
+        
+    def bottle_params
+        params.permit(:distillery_id, :name, :origin, :year, :price, :rating, :image_url)
+    end
+
 end
