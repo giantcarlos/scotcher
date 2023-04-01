4718e3447d9b7be539762cb560e0c55628eb6bb0
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-31 19:50:27 -0500

    migrations migrated

---
+++ db/schema.rb
@@ -0,0 +1,59 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_04_01_004620) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bottles", force: :cascade do |t|
+    t.string "name"
+    t.string "origin"
+    t.integer "year"
+    t.integer "price"
+    t.integer "rating"
+    t.string "image_url"
+    t.bigint "user_id", null: false
+    t.bigint "distillery_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["distillery_id"], name: "index_bottles_on_distillery_id"
+    t.index ["user_id"], name: "index_bottles_on_user_id"
+  end
+
+  create_table "distilleries", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "bottle_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bottle_id"], name: "index_notes_on_bottle_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bottles", "distilleries"
+  add_foreign_key "bottles", "users"
+  add_foreign_key "notes", "bottles"
+end
