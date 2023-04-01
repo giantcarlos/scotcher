230146a0a4707faabab9e82e6592bc456758876b
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-31 22:22:26 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+gian = User.create(username: "gian", email: "gianfakeemail@gmail.com", password: "1234")
+salem = User.create(username: "salem", email: "salemfakeemail@gmail.com", password: "1234")
+
+dalmore = Distillery.create(name: "Dalmore")
+macallan = Distillery.create(name: "Macallan")
+walker = Distillery.create(name: "Johnnie Walker")
+hibiki = Distillery.create(name: "Hibiki")
+grouse = Distillery.create(name: "Famous Grouse")
+laphroaig = Distillery.create(name: "Laphroaig")
+park = Distillery.create(name: "Higland Park")
+cooley = Distillery.create(name: "Cooley")
+macleod = Distillery.create(name: "Iam Macleod")
+
+Bottle.create(distiller_id: dalmore.id, user_id: gian.id, name: "Dalmore")
