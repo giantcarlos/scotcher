4aa2170b38f26607bb8ba14717cf844e5157a176
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-03-31 20:19:26 -0500

    add distillery controller actions

--- app/controllers/distilleries_controller.rb
+++ app/controllers/distilleries_controller.rb
@@ -1,2 +1,19 @@
 class DistilleriesController < ApplicationController
+
+    def index
+        distilleries = Distillery.all.order(:name)
+        render json: distilleries, status: :found
+    end
+    
+    def create
+        distillery = Distillery.create!(distillery_params)
+        render json: distillery, status: :created
+    end
+    
+    private
+    
+    def distillery_params
+        params.permit(:name, :image_url)
+    end
+
 end
