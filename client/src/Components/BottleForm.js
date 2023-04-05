2c77ca2ac8d6aa1d5aad38891664884c7f502b7e
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-04-05 18:06:40 -0500

    add login ternary to journal and add bottle

--- client/src/Components/BottleForm.js
+++ client/src/Components/BottleForm.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import Login from './Login';
 
 function BottleForm() {
+  const user = useSelector(state => state.sessions.entities)
+
+  if (!user) return <Login />;
+
   return (
     <div>BottleForm</div>
   )
--- client/src/Components/Journal.js
+++ client/src/Components/Journal.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import Login from './Login';
 
 function Journal() {
+  const user = useSelector(state => state.sessions.entities)
+
+  if (!user) return <Login />;
+
   return (
     <div>Journal</div>
   )
