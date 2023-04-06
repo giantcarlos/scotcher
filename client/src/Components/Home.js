9fb4c1d7634b5915a05c523774809e8a1cc86204
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-04-05 22:51:19 -0500

    add home message and sample bottles

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Login from './Login';
+import BottleCard from './BottleCard';
 
 function Home() {
     const user = useSelector(state => state.sessions.entities)
 
     if (!user) return <Login />;
 
+    const latestBottles = user.bottles.slice(Math.max(user.bottles.length -5, 0))
+    const bottleCards = () => latestBottles.map((bottle, key) => <BottleCard bottle={bottle} key={key}/>)
+
   return (
-    <div>Home</div>
+    <div>
+      <div className="counter"><br />Welcome {user.username}!<br /><br /><br /><br/>
+        <div>{latestBottles.length===0 ? "You don't have any logged scotch yet. Add some to your collection." : "Here are your highest rated bottles:"}</div>
+      </div>
+      <div className="card-grid">{bottleCards()}</div>
+    </div>
   )
 }
 
