2d4c48b9b3855d654c7fb55489a21ae0fdc93b65
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-04-06 20:34:19 -0500

    fix Home error

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -8,13 +8,13 @@ function Home() {
 
     if (!user) return <Login />;
 
-    const latestBottles = user.bottles.slice(Math.max(user.bottles.length -5, 0))
-    const bottleCards = () => latestBottles.map((bottle, key) => <BottleCard bottle={bottle} key={key}/>)
+    const latestBottles = user.bottles?.slice(Math.max(user.bottles?.length -5, 0))
+    const bottleCards = () => latestBottles?.map((bottle, key) => <BottleCard bottle={bottle} key={key}/>)
 
   return (
     <div>
       <div className="counter"><br />Welcome {user.username}!<br /><br /><br /><br/>
-        <div>{latestBottles.length===0 ? "You don't have any logged scotch yet. Add some to your collection." : "Here are your most recent scotch entries:"}</div>
+        <div>{latestBottles?.length===0 ? "You don't have any logged scotch yet. Add some to your collection." : "Here are your most recent scotch entries:"}</div>
       </div>
       <div className="card-grid">{bottleCards()}</div>
     </div>
