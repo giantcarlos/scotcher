fe01fcb123e7e309e4416978c69ba10438575c8a
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-04-12 07:53:12 -0500

    routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   resources :notes, only: [:index, :create]
   resources :bottles
   resources :users, only: [:show, :create]
@@ -15,5 +17,5 @@ Rails.application.routes.draw do
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
