b6b238c7450b4ee18b38d59eb145007888f35aba
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-04-13 13:04:01 -0500

    edit distillery model

--- app/models/distillery.rb
+++ app/models/distillery.rb
@@ -1,6 +1,6 @@
 class Distillery < ApplicationRecord
     has_many :bottles
-    has_many :users, through: :books
+    has_many :users, through: :bottles
 
     validates :name, presence: true, uniqueness: true
 end
